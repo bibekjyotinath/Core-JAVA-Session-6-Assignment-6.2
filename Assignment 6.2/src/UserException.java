69241fd3c1af145cb8fc966f032cdbd880bf5fca
bibekjyotinath <bibekjyotinath92@gmail.com>
2017-09-17 11:31:23 +0530

    Assignment 6.2
    Assignment 6.2

--- Assignment 6.2/src/UserException.java
+++ Assignment 6.2/src/UserException.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;       //Importing Scanner class for taking input from the user
 
 class MyException extends Exception{	  //creating a class for displaying user exception extending the Exception class
-	private short a;
+	private short a;		  //initializing of variable to character data type (private)
 	MyException(short age){           //Creating a constructor of the class MyException
-		a = age;
+		a = age;		  //assigning the value of age to a 
 	}
-	public String toString() {
+	public String toString() { .     //method created toString 
 		return "MyException [" +a+ "]";   //returns error message with the age 
 	}
 }
 
-public class UserException {
+public class UserException {		//class created UserException which holds the main method
 	
 	 static void validate(short age)throws MyException{  //method to check the age which extends MyEception class
 	     if(age<0)  							//Checking the condition
 	      throw new MyException(age);        //Throws exception if the condition fails
 	     else  
-	      System.out.println("The age is verified");  
+	      System.out.println("The age is verified");  		//if no exception has arrised prints the message on console
 	   }  
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		//main method of the program
 		
 		short age;						//initialization of age 
 		String name, email_id;			//initialization of name and email as character 
@@ -28,19 +28,19 @@ public class UserException {
 		
 		try							//the code that may arise problem is written in the try block
 		{
-			System.out.println("Enter your Name: ");
-			name = sc.next();
-			System.out.println("Enter your Age: ");
-			age = sc.nextShort();
-			System.out.println("Enter your Email: ");
-			email_id = sc.next();
-			validate(age);
+			System.out.println("Enter your Name: ");	//prints message on the console
+			name = sc.next();				//stores value taken from the user in name variable
+			System.out.println("Enter your Age: ");		//prints message on the console
+			age = sc.nextShort();				//Stores value taken from the user in age variable
+			System.out.println("Enter your Email: ");	//prints message on the console
+			email_id = sc.next();				//Stores value taken from the user in email_id variable
+			validate(age);					//calling the vaidate method  
 		}
 		catch(MyException me)			//catch block catches the exception that is handled by MyException class 
 		{
-			System.out.println("Age cannot be neagtive "+me);
+			System.out.println("Age cannot be neagtive "+me);	//prints message on the console
 		}
-		sc.close();
+		sc.close();				//closing the scanner class 
 		
 	}
 
